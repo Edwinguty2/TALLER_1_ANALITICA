24d6f21fdb3f0d373d09cf829419143d0ba84254
Usuario <Usuario@192.168.1.52>
2024-08-09 19:27:57 -0500

    anade interfaces

---
+++ src/main/java/edu/unisabana/pizzafactory/model/PizzaFactory.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package edu.unisabana.pizzafactory.model;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface PizzaFactory {
+    Amasador crearAmasador();
+    Moldeador crearMoldeador();
+    Horneador crearHorneador();
+}
